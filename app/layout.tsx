b0dd5c6847b675bf6bcec6984aaede511ce142cb
estrella <epopo2@illinois.edu>
2023-06-23 23:54:36 -0700

    add more content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from 'next-themes'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+ 
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+"use client"
 import Image from 'next/image'
+import GirlImage from '../public/girl_image.jpeg'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      
-      
+    <main className="flex min-h-screen flex-col mx-20 ">
+      <div className='flex flex-col items-center'>
+      <Image alt="" src={GirlImage} className='rounded-full mt-10 w-60'/>
+      </div>
+      <h1 className='mt-4 text-2xl'>congenial-fiesta</h1>
+      <p>thanks for stopping by! im exploring nextjs, and writing reviews of all the fizzy drinks I have every tried. </p>
+      <a className='mt-2'><button className='bg-violet-400 p-2 rounded-md'>who tf is ellie</button></a>
     </main>
   )
 }
