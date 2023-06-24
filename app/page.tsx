748ad3b9b731b48aa4cba852e2e737684a532db3
estrella <epopo2@illinois.edu>
2023-06-24 00:12:01 -0700

    add mobile first view of the website

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col mx-20 ">
       <div className='flex flex-col items-center'>
-      <Image alt="" src={GirlImage} className='rounded-full mt-10 w-60'/>
+      <Image alt="" src={GirlImage} className='rounded-full mt-40 w-60'/>
       </div>
-      <h1 className='mt-4 text-2xl'>congenial-fiesta</h1>
+      <h1 className='mt-4 text-4xl font-medium text-dark-purple'>congenial-fiesta</h1>
       <p>thanks for stopping by! im exploring nextjs, and writing reviews of all the fizzy drinks I have every tried. </p>
-      <a className='mt-2'><button className='bg-violet-400 p-2 rounded-md'>who tf is ellie</button></a>
+      <a className='mt-2 '><button className='bg-dark-purple text-white p-2 rounded-md'>who tf is ellie</button></a>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,20 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'dark-purple': '#6C538C',
+      'light-purple': '#E6D8E8',
+      'white': '#ffffff',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      fontFamily: {
+        Rubik: ['Rubik', 'sans-serif']
+      }
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
     },
   },
   plugins: [],
