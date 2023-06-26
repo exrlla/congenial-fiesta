01edf9676531bf6ff16ad9d0254730c65700f4e5
estrella <epopo2@illinois.edu>
2023-06-25 23:28:59 -0500

    more stuff push lol

---
+++ app/components/FriendIcon.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from 'react'
+import Image from 'next/image'
+import Ellie from '../../girl_image.jpeg'
+
+// type Props = {
+//     posts: string
+//     subreddit: string
+// }
+
+// const Posts: FunctionComponent<Props> = ({posts, })
+
+export default function FriendIcon() {
+    return(
+        <div>
+            <Image alt="" src={Ellie}></Image>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 "use client"
 import Image from 'next/image'
 import GirlImage from '../public/girl_image.jpeg'
+import FriendIcon from './components/FriendIcon'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col mx-20 ">
       <div className='flex flex-col items-center'>
-      <Image alt="" src={GirlImage} className='rounded-full mt-40 w-60'/>
+      <Image alt="" src={GirlImage} className='rounded-full mt-40 w-60' priority/>
       </div>
       <h1 className='mt-4 text-4xl font-medium text-dark-purple'>congenial-fiesta</h1>
-      <p>thanks for stopping by! im exploring nextjs, and writing reviews of all the fizzy drinks I have every tried. </p>
+      <p>thanks for stopping by! im exploring nextjs by creating a site where my friends and I can write fizzy drink soda reviews. </p>
       <a className='mt-2 '><button className='bg-dark-purple text-white p-2 rounded-md'>who tf is ellie</button></a>
+      <FriendIcon />
     </main>
   )
 }
